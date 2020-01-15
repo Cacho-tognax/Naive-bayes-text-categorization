f369e9cb025281cc0ecdd2f2ae234d17539aad14
Cacho-Tognax <fabio.tognaccini.96@gmail.com>
2020-01-15 17:21:39 +0100

    Addid .gitignore for raw data

--- Start.py
+++ Start.py
@@ -3,5 +3,3 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
-
-
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Naive bayes text categorization
+The Reuters-21578 has been renamed as Rcv1.
+For 20news the bydate version has been used.
