ec58479e56f664e75051cf882f40d889ac9aec83
Cacho-Tognax <fabio.tognaccini.96@gmail.com>
2020-01-25 23:19:57 +0100

    attempted using preprocessed rcv1V2 and kfolds, training set kept running in the AUC problem, giving up on AUC for this dataset

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,10 @@
 import numpy as np
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.naive_bayes import MultinomialNB
 
-training_test_sizes = [64, 128, 256, 512]
+training_test_sizes = [640]  # , 128, 256, 512]
 
 
 def evaluate(count_matrix, category, categories_count):
@@ -14,7 +15,24 @@ def evaluate(count_matrix, category, categories_count):
         acc_results = []
         for i in range(30):
             x_train, x_test, y_train, y_test = train_test_split(count_matrix, category,
-                                                                train_size=size, random_state=42 + i)
+                                                                train_size=size-categories_count)
+            '''  later if I have to do the force, rn let's just use the pre processed data
+            y_append = []
+            z_append = []  # indeces of elements to remove from test matrix
+            i = 0
+            while len(y_append) < categories_count and i < len(y_test):
+                if y_test[i] not in y_append:
+                    z_append.append(i)
+                    y_append.append(y_test[i])
+                i += 1
+            # after this we should have 1 element per category in the append set, could misbehave if all of category
+            # already in training set, implausible, in that case training set will be smaller
+            z_append = np.array(z_append)
+            x_append = x_test[z_append]
+            valids = np.arange(len(y_test))
+            x_test =
+            '''
+
             model = MultinomialNB()
             model.fit(x_train, y_train)
             classification = model.predict(x_test)
@@ -28,11 +46,60 @@ def evaluate(count_matrix, category, categories_count):
                 row[j] = 1
                 y_true.append(row)
             y_score = np.asarray(probability_classification)
-            auc_result = 0
+            auc_result = metrics.roc_auc_score(y_true, y_score, multi_class='ovo') * 100  # for percentages
+            '''
             try:
                 auc_result = metrics.roc_auc_score(y_true, y_score, multi_class='ovo')*100  # for percentages
             except ValueError:
                 pass
+            '''
+            if auc_result != 0:
+                auc_results.append(auc_result)
+        acc_performance.append([np.mean(acc_results), np.std(acc_results)])
+        auc_performance.append([np.mean(auc_results), np.std(auc_results)])
+    auc_perf = []
+    for pair in auc_performance:
+        auc_perf.append([format(pair[0], '.4f'), format(pair[1], '.4f')])
+    acc_perf = []
+    for pair in acc_performance:
+        acc_perf.append([format(pair[0], '.4f'), format(pair[1], '.4f')])
+    return auc_perf, acc_perf
+
+
+def evaluate_pre_split(train_count_matrix, train_category, test_count_matrix, test_category, categories_count):
+    train_category = np.array(train_category)
+    auc_performance = []
+    acc_performance = []
+    for size in training_test_sizes:
+        auc_results = []
+        acc_results = []
+        for i in range(30):
+            skf = StratifiedKFold(n_splits=int(np.floor(len(train_category)/size)), shuffle=True)
+            z_train = []
+            for test, train, in skf.split(train_count_matrix, train_category):
+                z_train = train
+            print('training test size offset = ' + str(abs(len(z_train) - size)))
+            x_train = train_count_matrix[z_train]
+            y_train = train_category[z_train]
+            model = MultinomialNB()
+            model.fit(x_train, y_train)
+            classification = model.predict(test_count_matrix)
+            probability_classification = model.predict_proba(test_count_matrix)
+            acc_results.append(np.mean(classification == test_category) * 100)  # for percentages
+            # now work only for roc
+            y_true = []
+            for j in test_category:
+                row = [0] * categories_count
+                row[j] = 1
+                y_true.append(row)
+            y_score = np.asarray(probability_classification)
+            auc_result = metrics.roc_auc_score(y_true, y_score, multi_class='ovo') * 100  # for percentages
+            '''
+            try:
+                auc_result = metrics.roc_auc_score(y_true, y_score, multi_class='ovo')*100  # for percentages
+            except ValueError:
+                pass
+            '''
             if auc_result != 0:
                 auc_results.append(auc_result)
         acc_performance.append([np.mean(acc_results), np.std(acc_results)])
--- parsers.py
+++ parsers.py
@@ -54,7 +54,7 @@ def text_data_parser(name):
         return 0, 0, 0
 
 
-def rcv1_parser(macro_category):
+def rcv1_parser_old(macro_category):  # we have to redo all this, I was using the old dataset.
     category_list = dict()
     with open('./Rcv1/rcv1-topics') as file:
         counter = 0
@@ -69,7 +69,7 @@ def rcv1_parser(macro_category):
             line = line.split(' ')
             if len(line) > 0 and category_list.get(line[0], 0) != 0:
                 category[line[1]] = category_list[line[0]]
-        all_text = []  # here we start parsing the documents
+    all_text = []  # here we start parsing the documents
     document_category = []
     files = os.listdir('./Rcv1')   # all files in the database directory
     data_sets = []
@@ -93,3 +93,72 @@ def rcv1_parser(macro_category):
     vectorizer = CountVectorizer(stop_words=stop_words_tuple)
     count_matrix = vectorizer.fit_transform(all_text)
     return count_matrix, document_category, category_list
+
+
+def rcv1_parser(macro_category):
+    category_list = dict()
+    with open('./Rcv1V2/rcv1-V2.topics.hier') as file:
+        counter = 0
+        for line in file:
+            line = word_tokenize(line)
+            if len(line) > 5 and line[2] == macro_category:
+                category_list[line[5]] = counter
+                counter += 1
+    category = dict()  # this uses the .qrels file to get the category for each document OLDID
+    with open('./Rcv1V2/rcv1-v2.topics.qrels') as file:
+        for line in file:
+            line = line.split(' ')
+            if len(line) > 0 and category_list.get(line[0], 0) != 0:
+                category[line[1]] = category_list[line[0]]
+    all_trains = []  # here we start parsing the documents
+    all_tests = []
+    train_document_category = []
+    test_document_category = []
+    current_document = ''
+    files = os.listdir('./Rcv1V2')  # all files in the database directory
+    train_data_sets = []
+    for file in files:
+        if 'train' in file:
+            train_data_sets.append(file)
+    test_data_sets = []
+    for file in files:
+        if 'test' in file:
+            test_data_sets.append(file)
+    for data_set in train_data_sets:
+        tex, cat = parse_lyrl(data_set, category)
+        all_trains.extend(tex)
+        train_document_category.extend(cat)
+    for data_set in test_data_sets:
+        tex, cat = parse_lyrl(data_set, category)
+        all_tests.extend(tex)
+        test_document_category.extend(cat)
+    vectorizer = CountVectorizer()
+    train_count_matrix = vectorizer.fit_transform(all_trains)
+    test_count_matrix = vectorizer.transform(all_tests)
+    return train_count_matrix, train_document_category, test_count_matrix, test_document_category, category_list
+
+
+def parse_lyrl(file_name, category):
+    texts = []
+    categories = []
+    with open('./Rcv1V2/' + file_name) as file:
+        mode = 0  # 0 looking for next document, 1 waiting for .W, 2 reading text
+        for line in file:
+            line = line.rstrip()
+            if mode == 0:
+                if '.I' in line:  # document found
+                    identifier = line.split(' ')[1]
+                    if category.get(identifier, 0) != 0:  # relevant document found
+                        mode = 1
+                        categories.append(category[identifier])
+            elif mode == 1:
+                if '.W' in line:
+                    mode = 2
+                    current_document = ''
+            elif mode == 2:
+                if len(line) > 0:
+                    current_document = current_document + ' ' + line
+                else:
+                    texts.append(current_document)
+                    mode = 0
+    return texts, categories
